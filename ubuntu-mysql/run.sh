f73e8040363da16c4441f212f4f0a97c31e4679e
tecris <eugen@cristea.org>
2015-03-02 12:24:23 +1300

    update for container link

--- ubuntu-mysql/run.sh
+++ ubuntu-mysql/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 docker run -d -p 23:22 -p 3306:3306 ubuntu/mysql
-#docker run -d -p 23:22 -p 63389:389 ubuntu/ldap
+#docker run -d -p 23:22 -p 3306:3306 ubuntu/mysql
+
+# set container name to make use of docker linking feature 
+# other containers can connect using 'docker run -d --link db:db ...'
+#docker run -d --name db -p 23:22 -p 3306:3306 ubuntu/mysql
 
