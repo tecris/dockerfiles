15745805ddcd4022f28a707c62044599184d86e3
tecris <eugen@cristea.org>
2015-03-02 18:16:33 +1300

    link param update

--- ubuntu-mysql/run.sh
+++ ubuntu-mysql/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-docker run -d -p 23:22 -p 3306:3306 ubuntu/mysql
-#docker run -d -p 23:22 -p 3306:3306 ubuntu/mysql
+# docker run -d -p 23:22 -p 3306:3306 ubuntu/mysql
+# docker run -d -p 23:22 -p 3306:3306 ubuntu/mysql
 
 # set container name to make use of docker linking feature 
-# other containers can connect using 'docker run -d --link db:db ...'
-#docker run -d --name db -p 23:22 -p 3306:3306 ubuntu/mysql
+# other containers can connect using 'docker run -d --link db:alias ...'
+docker run -d --name db -p 23:22 -p 3306:3306 ubuntu/mysql
 
