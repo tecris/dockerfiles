55eb0ed97c89a273063a6d83a51bf45dca298b60
tecris <eugen@cristea.org>
2015-03-06 20:22:07 +1300

    refactor to use official myslq dockerfile, https://github.com/docker-library/mysql/tree/master/5.7

--- ubuntu-mysql/run.sh
+++ ubuntu-mysql/run.sh
@@ -5,5 +5,5 @@
 
 # set container name to make use of docker linking feature 
 # other containers can connect using 'docker run -d --link db:alias ...'
-docker run -d --name db -p 23:22 -p 3306:3306 ubuntu/mysql
+docker run -d --name db -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=bookstore -e MYSQL_USER=bookstore -e MYSQL_PASSWORD=1bookstore! -p 23:22 -p 3306:3306 ubuntu/mysql
 
