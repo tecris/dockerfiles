671ba689fd920a6f04d8eede33015ad15eaabc65
tecris <eugen@cristea.org>
2015-05-10 22:22:16 +1200

    read image name from external file

--- oms-tomcat7/run.sh
+++ oms-tomcat7/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run --name omswood-web --link oms-db:db -d -p 24:22 -p 8080:8080 omswood/tomcat7
+. ./imageName
+
+docker run --name omswood-web --link oms-db:db -d -p 24:22 -p 8080:8080 $IMAGE_NAME
