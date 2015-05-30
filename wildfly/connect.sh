2178c8cbaec2dc4358716256f55cffdac07c2266
tecris <eugen@cristea.org>
2015-05-31 11:18:05 +1200

    ssh-less

--- wildfly/connect.sh
+++ wildfly/connect.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-
-#boot2docker ip
-ssh -p $1 root@localhost
-
+docker exec -it web bash
--- wildfly/run.sh
+++ wildfly/run.sh
@@ -2,4 +2,4 @@
 
 . ./imageName
 
-docker run -d --name web -p :22 -p 8080:8080 -p 9990:9990 $IMAGE_NAME
+docker run -d --name wildfly -p 8080:8080 -p 9990:9990 $IMAGE_NAME
