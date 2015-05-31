bfe313f974e5b0d4c0846239c7d2fa901665fe99
tecris <eugen@cristea.org>
2015-05-31 13:13:22 +1200

    update

---
+++ mysql/57/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+. ./imageName
+
+# set container name to make use of docker linking feature 
+# other containers can connect using 'docker run -d --link mysqldb:alias ...'
+docker run -d --name mysqldb -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_DATABASE=bookstore -e MYSQL_USER=bookstore -e MYSQL_PASSWORD=1bookstore! -p :22 -p 3306:3306 $IMAGE_NAME
+
