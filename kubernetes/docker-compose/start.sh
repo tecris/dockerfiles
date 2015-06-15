c6ea4ef35a9509e809c059d50c50a5da0304bde6
Eugen Cristea <eugen@cristea.org>
2015-06-16 11:43:30 +1200

    added check if port 808 in use

--- kubernetes/docker-compose/start.sh
+++ kubernetes/docker-compose/start.sh
@@ -1,4 +1,8 @@
 #/bin/bash
-
-
-docker-compose -f k8s.yml up -d
+SERVER=127.0.0.1 PORT=8080
+if (: < /dev/tcp/$SERVER/$PORT) 2>/dev/null
+then
+    echo "Port $PORT is already used, stop application using port $PORT, and run again this script"
+else
+    docker-compose -f k8s.yml up -d
+fi
