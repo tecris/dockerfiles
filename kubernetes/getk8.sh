17f3ca4716199d99a087e9b741c342df5f820dee
tecris <eugen@cristea.org>
2015-08-15 16:24:41 +1200

    added to repo

---
+++ kubernetes/getk8.sh
@@ -0,0 +1,11 @@
+
+VERSION=v1.0.1
+
+wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kube-controller-manager
+wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kube-api-server
+wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kube-scheduler
+wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kube-proxy
+wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kubelet
+wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kubectl
+
+
