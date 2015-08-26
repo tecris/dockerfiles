220840b1a8c65d4da02d6fc190637cfa75a78876
Eugen Cristea <eugen@cristea.org>
2015-08-26 14:31:29 +1200

    removed

--- kubernetes/docker-compose/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-docker kill $(docker ps -a | grep k8s | cut -c 1-12)
-docker rm -v $(docker ps -a | grep k8s | cut -c 1-12)
--- kubernetes/docker-compose/start.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-SERVER=127.0.0.1 PORT=8080
-if (: < /dev/tcp/$SERVER/$PORT) 2>/dev/null
-then
-    echo "Port $PORT is already used, stop application using port $PORT, and run again this script"
-else
-    docker-compose -f k8s.yml up -d
-fi
