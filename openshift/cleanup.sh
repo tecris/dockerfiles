7b083f7ed0307314c9f0f8b63a07b5aa1bb47777
Eugen Cristea <eugen@cristea.org>
2015-08-26 14:54:54 +1200

    removed openshift

--- openshift/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# http://fabric8.io/guide/openShiftDocker.html
-
-docker kill openshift
-docker rm -v openshift
-docker kill $(docker ps -a | grep k8s | cut -c 1-12)
-docker rm -v $(docker ps -a -q)
--- openshift/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# http://fabric8.io/guide/openShiftDocker.html
-
-bash <(curl -sSL https://bit.ly/get-fabric8) -m 192.168.1.66
