a214ca7424a565a1c16aa953ca78f20ef1ed8e3d
tecris <eugen@cristea.org>
2015-11-15 19:31:32 +1300

    added to repo

---
+++ pidgin/runPidgin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# to fix error: Gtk: cannot open display: unix:0
+xhost local:root
+
+# how to find mysql docker container ip address
+# docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' CONTAINER_NAME
+
+
+ docker run -ti --rm \
+ -v /tmp/.X11-unix:/tmp/.X11-unix \
+ -e DISPLAY=unix$DISPLAY \
+ org.tecris:5000/pidgin
