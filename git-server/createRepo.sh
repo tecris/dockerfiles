fd7d9fb00ad0d6143e36944cc9dff31b7bb588d2
tecris <eugen@cristea.org>
2015-11-16 22:11:21 +1300

    update

--- git-server/createRepo.sh
+++ git-server/createRepo.sh
@@ -6,7 +6,7 @@ BARE_REPO=$REPO_NAME.git
 mkdir $REPO_NAME
 
 # create local git repository and bare repository
-cd $REPO_NAME; git init; git add *; git commit -m '1st commit' *; cd ..; git clone --bare $REPO_NAME $BARE_REPO
+cd $REPO_NAME; echo "Ion Radan" >> ionradan.txt;git init; git add *; git commit -m '1st commit' *; cd ..; git clone --bare $REPO_NAME $BARE_REPO
 
 # copy bare repository to git server home directory
 cp -r $BARE_REPO /opt/docker_volumes/git/repositories
