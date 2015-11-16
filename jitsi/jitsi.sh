8e81dbadd0a4012235e23d58f1610c77c68f9b74
tecris <eugen@cristea.org>
2015-11-16 20:43:17 +1300

    added to repo

---
+++ jitsi/jitsi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# to fix error: Gtk: cannot open display: unix:0
+xhost local:root
+
+# how to find mysql docker container ip address
+# docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' CONTAINER_NAME
+
+docker run -ti --rm \
+	--net host \
+	--device=/dev/snd:/dev/snd \
+        -e DISPLAY=$DISPLAY \
+	-v /etc/machine-id:/etc/machine-id \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        org.tecris:5000/jitsi
