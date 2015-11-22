5ffc786fbd5e47e3fd2ea05dabf9de6b1701b507
tecris <eugen@cristea.org>
2015-11-22 18:06:23 +1300

    added to repo

---
+++ jenkins-cd/files/createJob.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ./createJob.sh 192.168.1.69 planets config.xml
+
+HOST=$1
+JOB_NAME=$2
+FILE=$3
+
+
+curl -X POST "http://$HOST:8080/createItem/createItem?name=$JOB_NAME" --data-binary "@$FILE" -H "Content-Type: text/xml"
