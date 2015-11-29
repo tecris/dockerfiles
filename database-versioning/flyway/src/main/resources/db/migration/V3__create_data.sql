d52e3723ba6777d339afacb6d5366bb81523e769
tecris <eugen@cristea.org>
2015-11-30 06:38:16 +1300

    added flyway sample project

---
+++ database-versioning/flyway/src/main/resources/db/migration/V3__create_data.sql
@@ -0,0 +1,31 @@
+INSERT INTO `bookstoredb`.`AUTHOR` (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('leo@author.email', 'Leonardo', 'Da Vinci');
+INSERT INTO `bookstoredb`.`AUTHOR` (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('danteauthor.email', 'Dante', 'Alighieri');
+INSERT INTO `bookstoredb`.`AUTHOR` (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('franc@author.email', 'Francesco', 'Pagano');
+INSERT INTO `bookstoredb`.`AUTHOR` (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('dimitrie@author.email', 'Dimitrie', 'Cantemir');
+INSERT INTO `bookstoredb`.`AUTHOR` (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('jlocke@author.email', 'John', 'Locke');
+INSERT INTO `bookstoredb`.`AUTHOR` (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('cmontesquieuo@author.email', 'Charles', 'Montesquieu');
+INSERT INTO `bookstoredb`.`AUTHOR` (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('tpaine@author.email', 'Thomas', 'Paine');
+INSERT INTO `bookstoredb`.`AUTHOR` (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('fschiller@author.email', 'Friedrich', 'Schiller');
+INSERT INTO `bookstoredb`.`AUTHOR` (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('jrousseau@author.email', 'Jean-Jacques', 'Rousseau');
+
+
+INSERT INTO `bookstoredb`.`GENRE` (GENRE) VALUES ('Comedy');
+INSERT INTO `bookstoredb`.`GENRE` (GENRE) VALUES ('Drama');
+INSERT INTO `bookstoredb`.`GENRE` (GENRE) VALUES ('Kids');
+INSERT INTO `bookstoredb`.`GENRE` (GENRE) VALUES ('Children');
+INSERT INTO `bookstoredb`.`GENRE` (GENRE) VALUES ('Fiction');
+INSERT INTO `bookstoredb`.`GENRE` (GENRE) VALUES ('SF');
+INSERT INTO `bookstoredb`.`GENRE` (GENRE) VALUES ('Investigation');
+INSERT INTO `bookstoredb`.`GENRE` (GENRE) VALUES ('Sport');
+INSERT INTO `bookstoredb`.`GENRE` (GENRE) VALUES ('Biography');
+
+
+INSERT INTO `bookstoredb`.`PUBLISHER` (EMAIL, NAME, PHONE) VALUES ('penguin@book.email', 'Penguin Books Ltd.', '044 90 12345670');
+INSERT INTO `bookstoredb`.`PUBLISHER` (EMAIL, NAME, PHONE) VALUES ('alpha@book.email', 'Alpha Books Ltd.', '044 90 12345671');
+INSERT INTO `bookstoredb`.`PUBLISHER` (EMAIL, NAME, PHONE) VALUES ('aladin@book.email', 'Aladdin Paperbacks', '044 90 12345672');
+INSERT INTO `bookstoredb`.`PUBLISHER` (EMAIL, NAME, PHONE) VALUES ('apress@book.email', 'Apress', '044 90 12345673');
+INSERT INTO `bookstoredb`.`PUBLISHER` (EMAIL, NAME, PHONE) VALUES ('tree@book.email', 'Long Tree', '044 90 12345674');
+INSERT INTO `bookstoredb`.`PUBLISHER` (EMAIL, NAME, PHONE) VALUES ('atlas@book.email', 'Altas Enterprise', '044 90 12345675');
+INSERT INTO `bookstoredb`.`PUBLISHER` (EMAIL, NAME, PHONE) VALUES ('cloud@book.email', 'Cloud Group', '044 90 12345676');
+INSERT INTO `bookstoredb`.`PUBLISHER` (EMAIL, NAME, PHONE) VALUES ('oblivion@book.email', 'Oblivion', '044 90 12345677');
+
