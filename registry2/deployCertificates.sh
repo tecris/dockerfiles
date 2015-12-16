cec030c300081213566591ce40a3b1a3ccadde5e
Eugen Cristea <eugen@cristea.org>
2015-12-17 11:05:08 +1300

    update

--- registry2/deployCertificates.sh
+++ registry2/deployCertificates.sh
@@ -8,5 +8,5 @@ echo $FQDN
 sudo mkdir -p /etc/docker/certs.d/$FQDN
 sudo cp certs/domain.crt /etc/docker/certs.d/$FQDN/ca.crt
 
-sudo mkdir -p /usr/local/docker/certs
-sudo cp certs/* /usr/local/docker/certs/
+sudo mkdir -p /opt/docker_volumes/registry/certs
+sudo cp certs/* /opt/docker_volumes/registry/certs/
