e5706bacec11a45537b71f39407347b049a77bd9
Eugen Cristea <eugen@cristea.org>
2015-12-24 20:03:41 +1300

    added no volume dockerfile

--- mysql/57/build.sh
+++ mysql/57/build.sh
@@ -6,3 +6,4 @@ echo  building $IMAGE_NAME
 
 # docker build -t $IMAGE_NAME .
 docker build --no-cache -t $IMAGE_NAME .
+docker build --no-cache -f Dockerfile.no_volumes -t casa.docker/mysql-no-volumes:5.7 .
