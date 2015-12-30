d4e345c7d903689071f477b70b6b6056d277f3d4
Eugen Cristea <eugen@cristea.org>
2015-12-31 00:01:35 +1300

    FQDN change for private registry to blue.sky, was bluesky

--- registry2/deployMirrorCertificates.sh
+++ registry2/deployMirrorCertificates.sh
@@ -1,14 +1,12 @@
 #1/bin/bash
 
-FQDN=$1
-# registry.mirror
-# bluesky
+FQDN=registry.mirror
 
 echo $FQDN
 
 # -- Configure docker to trust it
 sudo mkdir -p /etc/docker/certs.d/$FQDN
-sudo cp certs/$FQDN/domain.crt /etc/docker/certs.d/$FQDN/ca.crt
+sudo cp certs/$FQDN/domain.crt /etc/docker/certs.d/$FQDN:5000/ca.crt
 
 sudo mkdir -p /opt/docker_volumes/registry/$FQDN/certs
 sudo cp certs/$FQDN/* /opt/docker_volumes/registry/$FQDN/certs/
