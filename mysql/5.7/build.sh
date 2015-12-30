60dce0a1cd572eade7e95c065ef0f7665c770953
Eugen Cristea <eugen@cristea.org>
2015-12-30 19:23:01 +1300

    removed volume instruction from Dockerfile, if required use docker run -v ...

--- mysql/5.7/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. ./imageName
-
-echo  building $IMAGE_NAME
-
-# docker build -t $IMAGE_NAME .
-docker build --no-cache -t $IMAGE_NAME .
-docker build --no-cache -f Dockerfile.no_volumes -t casadocker/mysql-no-volumes:5.7 .
