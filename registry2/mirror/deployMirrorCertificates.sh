2ca782c60a1d755c146af23dfdb67e889091a518
Eugen Cristea <eugen@cristea.org>
2016-01-03 22:15:54 +1300

    certs path fix

--- registry2/mirror/deployMirrorCertificates.sh
+++ registry2/mirror/deployMirrorCertificates.sh
@@ -6,7 +6,7 @@ echo $FQDN
 
 # -- Configure docker to trust it
 sudo mkdir -p /etc/docker/certs.d/$FQDN:5000
-sudo cp certs/$FQDN/domain.crt /etc/docker/certs.d/$FQDN:5000/ca.crt
+sudo cp certs/domain.crt /etc/docker/certs.d/$FQDN:5000/ca.crt
 
 sudo mkdir -p /opt/docker_volumes/registry/$FQDN
 sudo cp -r certs /opt/docker_volumes/registry/$FQDN
