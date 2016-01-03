a846b2b516f35f0bf329e69ece10a8820f2ae01b
Eugen Cristea <eugen@cristea.org>
2016-01-03 16:15:35 +1300

    basic auth

--- registry2/mirror/deployMirrorCertificates.sh
+++ registry2/mirror/deployMirrorCertificates.sh
@@ -8,5 +8,6 @@ echo $FQDN
 sudo mkdir -p /etc/docker/certs.d/$FQDN:5000
 sudo cp certs/$FQDN/domain.crt /etc/docker/certs.d/$FQDN:5000/ca.crt
 
-sudo mkdir -p /opt/docker_volumes/registry/$FQDN/certs
-sudo cp certs/$FQDN/* /opt/docker_volumes/registry/$FQDN/certs/
+sudo mkdir -p /opt/docker_volumes/registry/$FQDN
+sudo cp -r certs /opt/docker_volumes/registry/$FQDN
+sudo cp -r auth  /opt/docker_volumes/registry/$FQDN
