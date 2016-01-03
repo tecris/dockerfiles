df38743be3535a8eefec6b8e523cbc2f9a85416d
Eugen Cristea <eugen@cristea.org>
2016-01-03 16:16:33 +1300

    update

--- registry2/private/deployBlueskyCertificates.sh
+++ registry2/private/deployBlueskyCertificates.sh
@@ -8,5 +8,5 @@ echo $FQDN
 sudo mkdir -p /etc/docker/certs.d/$FQDN
 sudo cp certs/$FQDN/domain.crt /etc/docker/certs.d/$FQDN/ca.crt
 
-sudo mkdir -p /opt/docker_volumes/registry/$FQDN/certs
-sudo cp certs/$FQDN/* /opt/docker_volumes/registry/$FQDN/certs/
+sudo mkdir -p /opt/docker_volumes/registry/$FQDN
+sudo cp -r certs /opt/docker_volumes/registry/$FQDN
