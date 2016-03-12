c51f12dfbd7d999dcdcae5ea424609a0858bfb73
Eugen Cristea <eugen@cristea.org>
2016-03-12 17:04:43 +0100

    rm -rf jenkins-base

--- jenkins-base/alpine/systemconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIRECTORYLIST="/usr/share/jenkins /usr/share/jenkins/ref ${JENKINS_SHARE} ${JENKINS_HOME} /usr/local/bin /usr/bin /opt/maven_config"
-
-for dir in ${DIRECTORYLIST}; do
-    mkdir -p ${dir}
-    chown -R jenkins:jenkins ${dir}
-    chmod -R 775 ${dir}
-done
--- jenkins-base/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-. ./imageName
-
-docker run --name jenkins --restart=always -d -p 8080:8080 $IMAGE_NAME
