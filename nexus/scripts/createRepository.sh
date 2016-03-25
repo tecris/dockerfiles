dc0668a4277403b442a639ce3fb78e3e1ccc9ba3
Eugen Cristea <eugen@cristea.org>
2016-03-25 11:23:21 +0200

    git rm -rf nexus pidgin

--- nexus/scripts/createRepository.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-HOST=$1
-FILE=$2
-PARAM=@$FILE
-
-# curl -i -H "Accept: application/xml" -H "Content-Type: application/xml" -f -X POST -v -d "@$(pwd)/insert_oss_repository.xml" -u admin:admin123 http://$HOST:8081/service/local/repositories
-curl -i -H "Accept: application/xml" -H "Content-Type: application/xml" -f -X POST -v -d "@$FILE" -u admin:admin123 http://$HOST:8081/service/local/repositories
-
