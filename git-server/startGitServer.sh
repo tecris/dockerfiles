42b0e6fdd1aac55695798b9e11972dff06d308d1
Eugen Cristea <eugen@cristea.org>
2016-05-03 13:43:56 +0300

    git add startGitServer.sh

---
+++ git-server/startGitServer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run --name git-server -d -p 2224:22 \
+	-v /opt/docker_volumes/git/.ssh:/home/git/.ssh \
+	-v /opt/docker_volumes/git/repositories:/opt/git/repositories \
+	casadocker/git-server:16.05.03
