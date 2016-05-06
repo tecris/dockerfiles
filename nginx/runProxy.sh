7f9db15ce944ebb38c53f685a2816c3ff6318c36
Eugen Cristea <eugen@cristea.org>
2016-05-06 10:02:59 +0300

    basic auth

--- nginx/runProxy.sh
+++ nginx/runProxy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker run -d --name nginx-reverse-proxy 
-    -v `pwd`/nginx.conf:/etc/nginx/conf.d/default.conf:ro 
-    -p 80:80 
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-password-authentication-with-nginx-on-ubuntu-14-04
+
+docker run -d --name nginx-reverse-proxy \
+    -v `pwd`/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
+    -v `pwd`/nginx.htpasswd:/etc/nginx/.htpasswd:ro  \
+    -p 80:80 \
     nginx:1.10
