ca8ff09b138c209ac78ea7d7a1dc3163a10c960c
Eugen Cristea <eugen@cristea.org>
2016-05-07 13:19:43 +0300

    Update startGitServer.sh

--- git-server/startGitServer.sh
+++ git-server/startGitServer.sh
@@ -3,4 +3,4 @@
 docker run --name git-server -d -p 2224:22 \
 	-v /opt/docker_volumes/git/.ssh:/home/git/.ssh \
 	-v /opt/docker_volumes/git/repositories:/opt/git/repositories \
-	casadocker/git-server:16.05.03
+	casadocker/git-server:16.05.07
