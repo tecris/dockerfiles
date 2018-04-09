282b7a7006931dfedea8ee52fa3478178a70f139
Eugen Cristea <eugen@cristea.org>
2018-04-09 08:11:11 +0200

    lets encripyt stuff

---
+++ nginx/runBlog.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-password-authentication-with-nginx-on-ubuntu-14-04
+
+docker run -d --name trivialis-blog \
+    -v `pwd`/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
+    -v /etc/letsencrypt/live/domain.org/fullchain.pem:/etc/letsencrypt/fullchain.pem:ro \
+    -v /etc/letsencrypt/live/domain.org/privkey.pem:/etc/letsencrypt/privkey.pem:ro \
+    -p 443:443 \
+    nginx
