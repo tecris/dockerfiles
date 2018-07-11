94ef8ec727c694a423c706577c2ac9aca53a1372
Eugen Cristea <eugen@cristea.org>
2018-07-11 22:58:34 +0200

    git add grafana

---
+++ grafana/5/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+# set -x
+
+url="http://$GF_SECURITY_ADMIN_USER:$GF_SECURITY_ADMIN_PASSWORD@localhost:3000"
+
+post() {
+    curl -s -X POST -d "$1" \
+        -H 'Content-Type: application/json;charset=UTF-8' \
+        "$url$2" 2> /dev/null
+}
+
+if [ ! -f "/var/lib/grafana/.init" ]; then
+    exec /run.sh $@ &
+
+    until curl -s "$url/api/datasources" 2> /dev/null; do
+      echo "Waiting for the URL to be available: $url"
+      sleep 1
+    done
+
+    for datasource in /etc/grafana/datasources/*; do
+      echo installed $datasource;
+      post "$(envsubst < $datasource)" "/api/datasources"
+    done
+
+    for dashboard in /etc/grafana/dashboards/*; do
+        post "$(cat $dashboard)" "/api/dashboards/db"
+    done
+
+    touch "/var/lib/grafana/.init"
+
+    kill $(pgrep grafana)
+    sleep 5
+fi
+
+exec /run.sh $@
