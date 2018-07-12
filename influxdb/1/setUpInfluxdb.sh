ab8585dd3f760ea02784d3f69a043fecf22c8a57
Eugen Cristea <eugen@cristea.org>
2018-07-12 21:44:28 +0200

    telegraf db and retention policy

--- influxdb/1/setUpInfluxdb.sh
+++ influxdb/1/setUpInfluxdb.sh
@@ -2,8 +2,14 @@
 
 HOST=localhost
 
-# create database in influxdb
+# create dcts-monitoring database in influxdb
 curl -XPOST http://${HOST}:8086/query --data-urlencode "q=CREATE DATABASE \"dcts-monitoring\""
 
-# create retention policy in influxdb
+# create dcts-monitoring retention policy in influxdb
 curl -XPOST http://${HOST}:8086/query --data-urlencode "q=CREATE RETENTION POLICY \"dcts-monitoring-retention\" ON \"dcts-monitoring\" DURATION 7d REPLICATION 1 DEFAULT"
+
+# create telegraf database in influxdb
+curl -XPOST http://${HOST}:8086/query --data-urlencode "q=CREATE DATABASE \"telegraf\""
+
+# create telegraf retention policy in influxdb
+curl -XPOST http://${HOST}:8086/query --data-urlencode "q=CREATE RETENTION POLICY \"telegraf-retention\" ON \"telegraf\" DURATION 7d REPLICATION 1 DEFAULT"
