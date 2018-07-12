2f81c278804e71db2a634c9bd8f426222230fbb6
Eugen Cristea <eugen@cristea.org>
2018-07-12 08:37:44 +0200

    cleanup, git rm -rf coinworld

--- coinworld/ltc/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo docker stack deploy -c docker-compose.yaml ltc
-
-# sudo docker exec -it ltc_litecoin-core.1.bvzrsbphcdlqv8ayoruir4blb litecoin-cli -rpcpassword=bar -rpcuser=foo help
-# sudo docker exec -it ltc_litecoin-core.1.bvzrsbphcdlqv8ayoruir4blb bash
-# docker stack ps ltc
-# sudo docker stack services ltc
-# sudo docker stack rm ltc
