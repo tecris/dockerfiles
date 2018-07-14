ac11dc28dbba392f6219a10729c6b1566c5d6820
Eugen Cristea <eugen@cristea.org>
2018-07-14 10:34:41 +0200

    config work

--- grafana/5/entrypoint.sh
+++ grafana/5/entrypoint.sh
@@ -1,34 +1,13 @@
 #!/usr/bin/env sh
 
-# set -x
+set -x
 
-url="https://localhost:3000"
 
-post() {
-    curl -u $GF_SECURITY_ADMIN_USER:$GF_SECURITY_ADMIN_PASSWORD \
-        -s -X POST -d "$1" \
-        -H 'Content-Type: application/json;charset=UTF-8' \
-        "$url$2" 2> /dev/null
-}
 
 if [ ! -f "/var/lib/grafana/.init" ]; then
-    exec /run.sh $@ &
-
-    sleep 5
-
-    for datasource in /etc/grafana/datasources/*; do
-      echo installed $datasource;
-      post "$(envsubst < $datasource)" "/api/datasources"
-    done
-
-    for dashboard in /etc/grafana/dashboards/*; do
-        post "$(cat $dashboard)" "/api/dashboards/db"
-    done
+    envsubst < /tmp/datasources_template.yaml > /etc/grafana/provisioning/datasources/datasources.yaml
 
     touch "/var/lib/grafana/.init"
-
-    kill $(pgrep grafana)
-    sleep 5
 fi
 
 exec /run.sh $@
