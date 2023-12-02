38f134bc9fb125ee6dd347c056d6b92489f52af8
tecristea <tecristea@gmail.com>
2023-12-02 19:39:55 +1300

    systemd bashing

--- systemd/build_image.sh
+++ systemd/build_image.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 #
-set -x
+# set -x
 
-if [ "$#" -ne 2 ]; then
-    echo -e "Usage: $0 <os> <version>"
-    echo -e "Example: $0 ubuntu 22.04"
-    exit
-fi
+declare -A distrubution_array
+distrubution_array[debian]="10,11"
+distrubution_array[ubuntu]="18.04,20.04,22.04"
 
-os=${1}
-os_version=${2}
 
-docker buildx build \
-    --no-cache \
-    --progress=plain \
-    -t org.tecris/${os}_${os_version}-systemd:23.08.04 \
-    -f ${os}-${os_version}/Dockerfile \
-    .
+for distribution in "${!distrubution_array[@]}"
+do
+  os=${distribution}
+  os_version_list=${distrubution_array[${distribution}]}
+  echo "os : ${distribution}" -- "os_version_list: ${os_version_list}"
+  for os_version in ${distrubution_array[${distribution}]//,/ }
+  do
+    echo "os : ${distribution}" -- "os_version: ${os_version}"
+    IMAGE_NAME=org.tecris/${os}_${os_version}-systemd:23.12.02
+    echo ${IMAGE_NAME}
+
+    docker buildx build \
+      --no-cache \
+      --progress=plain \
+      -t ${IMAGE_NAME} \
+      -f ${os}-${os_version}/Dockerfile \
+      ./
+
+    docker buildx build \
+      --load \
+      -t ${IMAGE_NAME} \
+      ./${os}-${os_version}
+  done
+
+done
+
