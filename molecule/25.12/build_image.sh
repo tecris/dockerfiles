986d4939482d234891a538d0137f06d563a61fd0
tecristea <tecristea@gmail.com>
2026-01-25 20:29:39 +1300

    molecule git add molecule/25.12

---
+++ molecule/25.12/build_image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE_TAG=$(date +"%y.%m.%d")
+IMAGE_NAME=org.tecris/molecule:${IMAGE_TAG}
+
+docker buildx build \
+    --no-cache \
+    --progress=plain \
+    --load \
+    -t ${IMAGE_NAME} \
+    -f Dockerfile \
+    ./
