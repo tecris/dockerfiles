c91bc118f0a8436024c03aefb933982d2534b245
tecristea <tecristea@gmail.com>
2026-03-22 21:03:31 +1300

    NOTASK drop ubuntu 20.04, add debian 13

--- systemd/build_images.sh
+++ systemd/build_images.sh
@@ -24,8 +24,8 @@ function build_image() {
 }
 
 declare -A distrubution_array
-distrubution_array[debian]="11,12"
-distrubution_array[ubuntu]="20.04,22.04,24.04"
+distrubution_array[debian]="11,12,13"
+distrubution_array[ubuntu]="22.04,24.04"
 
 for distribution in "${!distrubution_array[@]}"
 do
